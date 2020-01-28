defb2a66035a576262b6fab82f8290558b037836
louisc59 <60093606+louisc59@users.noreply.github.com>
2020-01-28 14:33:28 +0100

    Add files via upload

---
+++ ICandidat.java
@@ -0,0 +1,7 @@
+package com.inti.dao;
+
+public interface ICandidat {
+	
+	public void postuler();
+	public void contact();
+}
---
+++ ICandidatImpl.java
@@ -0,0 +1,19 @@
+package com.inti.dao.impl;
+
+import com.inti.dao.ICandidat;
+
+public class ICandidatImpl implements ICandidat {
+
+	@Override
+	public void postuler() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contact() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
