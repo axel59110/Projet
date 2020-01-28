525c7ebbcfcb9bbed2d425fef3ed7f8910d5133f
axel59110 <60090747+axel59110@users.noreply.github.com>
2020-01-28 14:00:29 +0100

    Add files via upload

--- Formateur.java
+++ Formateur.java
@@ -1,3 +1,123 @@
-public class Formateur{
+import java.util.Arrays;
 
+public class Formateur {
+	int Id;
+	String nom;
+	String prénom;
+	String spécialité; 
+	String ville;
+	String matière;
+	String commentaire;
+	Float note;
+	Float moyenne;
+	
+	float tableaunote []= {note};
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrénom() {
+		return prénom;
+	}
+
+	public void setPrénom(String prénom) {
+		this.prénom = prénom;
+	}
+
+	public String getSpécialité() {
+		return spécialité;
+	}
+
+	public void setSpécialité(String spécialité) {
+		this.spécialité = spécialité;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getMatière() {
+		return matière;
+	}
+
+	public void setMatière(String matière) {
+		this.matière = matière;
+	}
+
+	public String getCommentaire() {
+		return commentaire;
+	}
+
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+
+	public Float getNote() {
+		return note;
+	}
+
+	public void setNote(Float note) {
+		this.note = note;
+	}
+
+	public Float getMoyenne() {
+		return moyenne;
+	}
+
+	public void setMoyenne(Float moyenne) {
+		this.moyenne = moyenne;
+	}
+
+	public float[] getTableaunote() {
+		return tableaunote;
+	}
+
+	public void setTableaunote(float[] tableaunote) {
+		this.tableaunote = tableaunote;
+	}
+
+	@Override
+	public String toString() {
+		return "Formateur [Id=" + Id + ", nom=" + nom + ", prénom=" + prénom + ", spécialité=" + spécialité + ", ville="
+				+ ville + ", matière=" + matière + ", commentaire=" + commentaire + ", note=" + note + ", moyenne="
+				+ moyenne + ", tableaunote=" + Arrays.toString(tableaunote) + "]";
+	}
+
+	public Formateur() {
+		super();
+	}
+
+	public Formateur(int id, String nom, String prénom, String spécialité, String ville, String matière,
+			String commentaire, Float note, Float moyenne, float[] tableaunote) {
+		super();
+		Id = id;
+		this.nom = nom;
+		this.prénom = prénom;
+		this.spécialité = spécialité;
+		this.ville = ville;
+		this.matière = matière;
+		this.commentaire = commentaire;
+		this.note = note;
+		this.moyenne = moyenne;
+		this.tableaunote = tableaunote;
+	}
+	
+	
 }
---
+++ FormateurTravail.java
@@ -0,0 +1,74 @@
+
+public class FormateurTravail implements IFormateurTravail {
+Formateur formateur=new Formateur();
+	
+
+	@Override
+	public void DeleteNote() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Formateur UpdateNote() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void CalculMoyenne() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Formateur AjouterCommentaire() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void DeleteCommentaire() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Formateur UpdateCommentaire() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void TerminerMatiere() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void AvancéeMatière() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void WarningEtudiant() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void FindbyId(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Formateur AjouterNote() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+}
